a44a78442906fe3878166d6512f7fe735e162bb8
Ran Ren <ranforrestren@gmail.com>
2022-05-30 19:24:15 -0400

    Add basic mouseover color change functionality

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,15 @@ function makeGrid(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < rows * cols; i++) {
     const gridBox = document.createElement('div');
+    gridBox.addEventListener('mouseover', colorChange);
     gridBox.classList.add('gridBox');
     container.appendChild(gridBox);
   }
+}
+
+function colorChange(e) {
+  console.log('Test');
+  e.target.style.background = 'blue';
 }
 
 makeGrid(16, 16);
