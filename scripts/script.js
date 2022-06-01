fbd3fe258705f1391019c0eeaca3ddd07efa2a5b
Ran Ren <ranforrestren@gmail.com>
2022-05-31 20:32:37 -0400

    Fix unintended selection behavior

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 let isMouseDown = false;
 
+// Records mousedown/mouseup state
 document.addEventListener("mousedown", () => {
   isMouseDown = true;
 })
@@ -15,17 +16,23 @@ function makeGrid(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < rows * cols; i++) {
     const gridBox = document.createElement('div');
-    gridBox.addEventListener('mouseover', colorChange);
-    gridBox.classList.add('gridBox');
+    gridBox.addEventListener('mouseover', colorChangeHover);
+    gridBox.addEventListener('mousedown', colorChangeDown);
+    gridBox.classList.add('gridBox', 'unselectable');
     container.appendChild(gridBox);
   }
 }
 
 // Changes color of square on mouseover
-function colorChange(e) {
+function colorChangeHover(e) {
   if (isMouseDown == true) {
     e.target.style.background = 'blue';
   }
+}
+
+// Changes color of square on click
+function colorChangeDown(e) {
+  e.target.style.background = 'blue';
 }
 
 makeGrid(16, 16);
